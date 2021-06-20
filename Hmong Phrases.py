e301d0cebd0131919bce4d8bc0499f4b38fcb71e
zjones3205 <51040129+zjones3205@users.noreply.github.com>
2021-06-20 16:51:49 -0600

    Add files via upload

---
+++ Hmong Phrases.py
@@ -0,0 +1,84 @@
+
+Eng2Hmong {'Hello!': 'Nyob Zoo!',
+'what is your name?': 'Koj lub npe hu li cas?',
+'What are you going [to be up to]?': 'Koj mus (d)abtsi?',
+'What do you think?': 'Puas xav li cas?',
+'Did/do you know?': 'Puas paub?',
+'Are you good?': 'Puas zoo?',
+"I'm happy.": 'Kuv zoo siab.',
+'Where are you going?': '[Koj] mus qhov twg?',
+'Are you Hmong?': 'Puas yog Hmoob?',
+'Do you have [it]?': 'Puas muaj?',
+'I know the Hmong language.': 'Kuv paub lus Hmoob.',
+'Do you know me?': 'Koj puas paub kuv?',
+'What year is your birthday?': 'Koj hnug yug xyoo twg?',
+"What's your nationality?": 'Koj yog neeg abtsi?',
+'What did you say?': 'Koj hais (d)abtsi',
+"I'll call you.": 'Kuv hu koj.',
+'When are you coming?': 'Koj puas tuaj?',
+"Who's calling?": 'Leejtwg hu li cas?',
+'Happy New Year!': 'Nyob Zoo Xyoo Tshiab!',
+'I wait only for you. (romantic)': 'Kuv tos koj xwb.',
+"No problem / Don't mention it": 'tsis ua li cas.',
+'Do you want to eat': 'Koj puas xav noj mov?',
+'Is it also good?': 'Puas zoo thiab?',
+'Is it fun?': 'Puas lom zem?',
+'God loves you': 'Vaj tswv hlub koj',
+'May God bless you.': 'Cia Vaj tswv foom koob hmoov rau koj.',
+'[Give] Thanks to God': 'Ua Vaj tswv tsaug.',
+'Let us pray [to God].': 'Cia peb thov Vaj tswv',
+"Come, we're going to eat.": 'Tuaj peb noj mov.',
+'Come, we are studying Hmong': 'tuaj peb kawm lus Hmoob.',
+"I'm talking to you.": 'Kuv his koj.',
+'I like you.': 'Kuv nyiam koj.',
+'I love you!': 'Kuv hlub koj!',
+"I know what you're getting at [by saying that]": 'Kuv paub koj hais ko mas.',
+'Addressing me?': 'Hais kuv?',
+'Once upon a time...': 'Puag puag thaum ub...',
+"What's your last name": 'Koj yog xeem abtsi?',
+'Can you help me?': 'Koj pab kuv, puas tau?'
+}
+
+# Some Linguistics Notes of Intrigue
+
+# Dabtsi and abtsi are identical in meaning.  However dab means demon/evil spirit,
+# so you are slanting your wording/question to indicate something negative, troublesome,
+# or naughty should you say dabtsi instead of abtsi.  The Hmong can be very superstitious, so
+# please use care and thoughtfulness around elders in how you use this seemingly small nuance.
+# When talking to children, you are insinuating they are being a cheeky troublemaker, which
+# is more or less innocent- particularly if its clear you are teasing them.
+
+# Puas is a functional word that transforms any phrase/action into a question.
+
+# Hmong does not has a discrete Yes / No in the language.  You either affirm or negate the
+# verb or action using mas as suffix to the verb or using tsis as prefix to the verb.
+# Yog mas is an all around general affirmation and is the closest phrase to Yes, though it
+# colloquially translates into something more like "that's correct".
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
